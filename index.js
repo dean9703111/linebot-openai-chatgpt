b2d7aa90054b461b29b9d8deb18ba4985f5aa15f
Dean Lin <dean@jet-link.com.tw>
2023-01-24 19:51:50 +0800

    test

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 require('dotenv').config();
-const { Configuration, OpenAIApi } = require("openai");
+
+const express = require('express');
 const line = require('@line/bot-sdk');
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+
+// create LINE SDK config from env variables
 const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
+
+// create LINE SDK client
 const client = new line.Client(config);
 
-const express = require('express');
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
-const port = 3000;
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).end();
-        });
-});
-app.listen(port, () => {
-    console.log(`app is running on port ${port}`)
-})
-
-function handleEvent (event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
-    return openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: event.message.text,
-    }).then(response => {
-        return client.replyMessage(event.replyToken, {
-            type: 'text',
-            text: response.data.choices[0].text
-        });
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
     });
+});
+
+// event handler
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text ,
+    max_tokens: 500,
+  });
+
+  // create a echoing text message
+  const echo = { type: 'text', text: completion.data.choices[0].text.trim() };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
 }
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
